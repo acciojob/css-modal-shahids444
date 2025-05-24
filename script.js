089eae48fdd13cb434e98dc9f5d799eff1489886
shahids444 <shahidshaiks444@gmail.com>
2025-05-25 00:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+document.querySelector("#openModal").addEventListener("click",()=>{
+	document.querySelector(".modal").classList.remove("hidden")
+})
+document.querySelector(".close-modal").addEventListener("click",()=>{
+	document.querySelector(".modal").classList.add("hidden")
+})
+
